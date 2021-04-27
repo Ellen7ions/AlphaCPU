45c02ca9714c93f366cb0dd19bfaeb041a622ac1
Ellen7ions <lzz765177255@gmail.com>
2021-04-27 14:34:06 +0800

    nothing

---
+++ ip_core/Dis_Memory/Dis_Memory_stub.vhdl
@@ -0,0 +1,34 @@
+-- Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+-- Date        : Wed Apr 14 20:08:18 2021
+-- Host        : DESKTOP-27OI68B running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               f:/University/LoogSonSrc/CPULabs/Lab2_PC/Lab2_PC.srcs/sources_1/ip/Dis_Memory/Dis_Memory_stub.vhdl
+-- Design      : Dis_Memory
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a200tfbg676-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Dis_Memory is
+  Port ( 
+    a : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    d : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    clk : in STD_LOGIC;
+    we : in STD_LOGIC;
+    spo : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end Dis_Memory;
+
+architecture stub of Dis_Memory is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "a[9:0],d[31:0],clk,we,spo[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "dist_mem_gen_v8_0_13,Vivado 2020.1";
+begin
+end;
